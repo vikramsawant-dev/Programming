3a63591221049a3f7ee7b482f8b428b72aadcb73
Vikram Ramesh Sawant <vikramsawantpatil0@gmail.com>
2026-08-25 23:13:39 +0530

    Add files via upload

---
+++ Java_Programming/Practice/program93.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+//Pallindrome
+class DigitX
+{
+    public boolean Checkpallindrome(int iNo)
+    {
+        int iDigit = 0;
+        int iRev = 0;
+        int iTemp = 0;
+
+        iTemp = iNo;
+        while(iNo !=0)
+        {
+            iDigit = iNo % 10;
+            iRev = (iRev * 10) + iDigit;
+            iNo = iNo / 10;
+        }
+        if(iRev == iTemp )
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+
+        }
+        
+
+    }
+
+}
+
+class program93
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        DigitX dobj = new DigitX();
+
+        int iValue = 0;
+        boolean bRet = false;
+
+        System.out.println("Enter Number :");
+        iValue = sobj.nextInt();
+
+        bRet = dobj.Checkpallindrome(iValue);
+        
+        if(bRet == true)
+        {
+            System.out.println("Number is Pallindrome");
+
+        }
+        else
+        {
+            System.out.println("Number is not Pallindrome");
+        }
+
+    }
+}
