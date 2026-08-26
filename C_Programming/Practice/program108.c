4bdb25a024f4c7b9b6e8962312c10451c26d046b
Vikram Ramesh Sawant <vikramsawantpatil0@gmail.com>
2026-08-27 01:52:50 +0530

    Add files via upload

---
+++ C_Programming/Practice/program108.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void Display(int Arr[])
+{
+    printf("%d\n",Arr[0]);
+
+    Arr++;
+    printf("%d\n",*Arr);
+
+    Arr++;
+    printf("%d\n",*Arr);
+
+}
+
+int main()
+{
+    int Brr[5] = {10,20,30,40,50};
+
+    Display(Brr);
+
+    return 0;
+}
