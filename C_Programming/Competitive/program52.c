7c720c7619b07c76c13b9184c18bcb799670cc32
Vikram Ramesh Sawant <vikramsawantpatil0@gmail.com>
2026-09-04 01:33:13 +0530

    Add files via upload

---
+++ C_Programming/Competitive/program52.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int BOOL;
+
+BOOL ChkZero(int iNo)
+{
+    int iDigit = 0;
+    
+    if (iNo < 0)
+    {
+       iNo = -iNo;
+    }
+    while(iNo != 0)
+    {
+        iDigit = iNo % 10;
+        
+        if(iDigit  == 0)
+        {
+            return TRUE;
+        }
+        iNo = iNo / 10;
+    }
+    return FALSE;
+
+}
+int main()
+{
+    int iValue = 0;
+    BOOL bRet = FALSE;
+   
+
+    printf("Enter the number : ");
+    scanf("%d",&iValue);
+
+    bRet = ChkZero(iValue);
+
+    if(bRet == TRUE)
+    {
+        printf("It contains zero\n");
+    }
+    else
+    {
+        printf("There is no zero\n");
+    }
+
+    return 0;
+}
---
+++ C_Programming/Competitive/program56.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int Difference(int Arr[] , int iLength)
+{
+    int iCnt = 0;
+    int iEvenSum = 0;
+    int iOddSum = 0;
+    
+    for(iCnt = 0; iCnt < iLength; iCnt++)
+    {
+        if((Arr[iCnt] % 2 )== 0)
+        {
+            iEvenSum = iEvenSum + Arr[iCnt];
+        }
+        else
+        {
+            iOddSum = iOddSum + Arr[iCnt];
+        }
+    }
+    return (iEvenSum - iOddSum);
+
+}
+int main()
+{
+    int iSize = 0;
+    int iCnt = 0;
+    int iRet = 0;
+    int *p = NULL;
+   
+    printf("Enter the number of elements : \n");
+    scanf("%d",&iSize);
+
+    p = (int *)malloc(iSize * sizeof(int));
+
+    if(p == NULL)
+    {
+        printf("Unable to allocate memory");
+        return -1;
+    }
+    printf("Enter %d elements\n",iSize);
+    
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        printf("Enter elements : %d\n",iCnt+1);
+        scanf("%d",&p[iCnt]);
+    }
+    iRet = Difference(p,iSize);
+    printf("Result is %d\n",iRet);
+
+    return 0;
+}
