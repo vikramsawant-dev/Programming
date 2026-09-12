c7108464ce033b5202a437b5250cfb8fecfc8e32
Vikram Ramesh Sawant <vikramsawantpatil0@gmail.com>
2026-09-12 17:36:16 +0530

    Add files via upload

---
+++ Java_Programming/Practice/program192.java
@@ -0,0 +1,42 @@
+//Input: 4
+//Output: * * * *
+//        * * * *
+//        * * * *
+//        * * * *
+
+
+import java.util.*;
+
+class program192
+{
+    public static void Display(int iRow, int iCol)
+    {
+        int i = 0;
+        int j = 0;
+
+        for(i = 1; i <= iRow; i++)
+        {
+            for(j = 1; j <= iCol; j++)
+            {
+                System.out.print("*\t");
+            }
+            System.out.println();
+        }
+
+
+    }
+    public static void main(String A[] )
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue1 = 0;
+        int iValue2 = 0;
+
+        System.out.println("Enter the number of rows :");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter the number of column :");
+        iValue2 = sobj.nextInt();
+        
+        Display(iValue1,iValue2);
+    }
+}
